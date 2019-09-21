6a06e6dc03a30d1adc65cd36d467486727930549
Kent vejrup Madsen <20760795+KentMadsen@users.noreply.github.com>
2019-09-21 17:17:26 +0200

    a

--- Script/upload.py
+++ Script/upload.py
@@ -4,39 +4,117 @@ import mysql.connector
 import os
 
 mydb = mysql.connector.connect(
-    host="",
-    user="",
-    passwd="",
-    database=""
+   # host="",
+  #  user="",
+ #   passwd="",
+#    database=""
 )
 
-search = "/home/madsen/Logs"
+search = "D:\\DataSets\\usenetcorpus\\"
 
 files = []
 
 
-def getContent(file):
-    f = open(file, "r")
+#def getContent(file):
+#    f = open(file, "r",  encoding="utf-8", errors="ignore")
+#
+#    contents = f.read()
 
-    contents = f.read()
-    print(contents)
+#    f.close()
+
+#    return contents
+
+
+#for r, d, f in os.walk(search):
+ #   for file in f:
+  #      full = os.path.join(r, file)
+   #     parent = os.path.abspath(os.path.join(full, os.pardir))
+
+    #    sql = "INSERT INTO dailymail_stories(directory, name, content) values(%s, %s, %s)"
+     #   value = (parent, file, getContent(full))
+
+      #  cursor = mydb.cursor()
+       # cursor.execute(sql, value)
+
+        #mydb.commit()
+
+
+# sql = "INSERT INTO usenet_corpus(name, content) values( %s, %s )"
+
+# cursor = mydb.cursor()
+# cursor.execute(sql, value)
+
+# mydb.commit()
+
+def getContent(file):
+    f = open(file, "r",  encoding="utf-8", errors="ignore")
+
+    # Reads the first line
+    line = f.readline()
+
+    buffer = None
+
+    current_pos = 0
+    records = []
+
+    while line:
+
+        if buffer is None:
+            buffer = line
+        else:
+            buffer = buffer + line
+
+        if "---END.OF.DOCUMENT---" in line:
+            value = (file, buffer)
+
+            records.append(value)
+
+            buffer = None
+
+            current_pos = current_pos + 1
+
+            if (current_pos % 10000) == 0:
+                print(current_pos)
+
+            if current_pos < 32880000:
+                records = []
+
+            if len(records) > 10000 and current_pos > 32880000:
+                sql = """INSERT INTO usenet_corpus(name, content) values( %s, %s )"""
+
+                cursor = mydb.cursor()
+                cursor.executemany(sql, records)
+
+                mydb.commit()
+
+                records = []
+
+        line = f.readline()
+
+    if len(records) != 0:
+        sql = """INSERT INTO usenet_corpus(name, content) values( %s, %s )"""
+
+        cursor = mydb.cursor()
+        cursor.executemany(sql, records)
+
+        mydb.commit()
+
 
     f.close()
 
-    return contents
 
 
 for r, d, f in os.walk(search):
     for file in f:
-        files.append(os.path.join(r, file))
+        full = os.path.join(r, file)
+        parent = os.path.abspath(os.path.join(full, os.pardir))
 
-for f in files:
-    print(f)
+        getContent(full)
 
-    sql = "INSERT INTO raw_files(parent, title, content) values(%s, %s, %s)"
-    value = ( os.path.abspath(os.path.join(f, os.pardir)), f, getContent(f))
+        #sql = "INSERT INTO dailymail_stories(directory, name, content) values(%s, %s, %s)"
+        #value = (parent, file, getContent(full))
 
-    cursor = mydb.cursor()
-    cursor.execute(sql, value)
+        #cursor = mydb.cursor()
+        #cursor.execute(sql, value)
 
-    mydb.commit()
+        #mydb.commit()
